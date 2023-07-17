2ff7aacba3be55b1ba4aa698de25e27c3f5f84cf
Shammael <shammamnd2015@gmail.com>
2023-07-17 11:58:57 -0400

    feat: :fire: Remove episodesID[]

--- src/modules/character/dtos/requests/update_character.request.dto.ts
+++ src/modules/character/dtos/requests/update_character.request.dto.ts
@@ -1,15 +1,17 @@
 import { CharacterGender, CharacterStatus, Specy } from '@prisma/client';
-import { IsArray, IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateCharacterRequestDto {
   @IsString()
+  @IsOptional()
   name: string;
   @IsEnum(CharacterStatus)
+  @IsOptional()
   status: CharacterStatus;
   @IsEnum(Specy)
+  @IsOptional()
   specy: Specy;
   @IsEnum(CharacterGender)
+  @IsOptional()
   gender: CharacterGender;
-  @IsArray()
-  episodeIDs: string[];
 }
