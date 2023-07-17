2869516fefd6816eff0ccc60c5eccc7d45315bfb
Shammael <shammamnd2015@gmail.com>
2023-07-17 11:57:21 -0400

    feat: :hammer: Add GetAll method to controller

--- src/modules/character/character.controller.ts
+++ src/modules/character/character.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
-import { CharacterService } from './character.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CharacterState, CharacterStatus, Specy } from '@prisma/client';
+
+import { CharacterService, Filter } from './character.service';
 import { CreateCharacterRequestDto, UpdateCharacterRequestDto } from './dtos';
+import { GetAllParamsDtos } from './dtos/requests/get_all.request.dto';
 
 @Controller('character')
 export class CharacterController {
@@ -18,5 +30,36 @@ export class CharacterController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() request: UpdateCharacterRequestDto) {
     return this.characterService.update(id, request);
+  }
+
+  @Patch('assign/:id')
+  asignEpisode(@Param('id') id: string, @Body() input: string[]) {
+    return this.characterService.asignEpisodes(id, input);
+  }
+
+  @Get()
+  getAll(@Query() input: GetAllParamsDtos) {
+    let filter: Filter;
+
+    switch (input.filter_type) {
+      case 'specy':
+        filter = { type: 'specy', value: input.filter_value as Specy };
+        break;
+      case 'status':
+        filter = {
+          type: 'status',
+          value: input.filter_value as CharacterStatus,
+        };
+        break;
+      case 'state':
+        filter = { type: 'state', value: input.filter_value as CharacterState };
+        break;
+    }
+
+    return this.characterService.getAll({
+      ...filter,
+      limit: parseInt(input.limit),
+      page: parseInt(input.page),
+    });
   }
 }
