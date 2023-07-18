7c839e9e3ed1f7d4af3273ed330de47ac528b36d
Shammael <shammamnd2015@gmail.com>
2023-07-17 22:06:18 -0400

    feat: :hammer: Add Dynamic_value in his own folder

--- src/modules/character/dtos/requests/get_all.request.dto.ts
+++ src/modules/character/dtos/requests/get_all.request.dto.ts
@@ -1,68 +1,7 @@
-import { BadRequestException, HttpStatus } from '@nestjs/common';
 import { CharacterState, CharacterStatus, Specy } from '@prisma/client';
 import { Transform } from 'class-transformer';
-import {
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-  registerDecorator,
-  ValidationArguments,
-  ValidationOptions,
-} from 'class-validator';
-
-function DynamicFilterValue(validationOptions?: ValidationOptions) {
-  return function (object: any, propertyName: string) {
-    registerDecorator({
-      name: 'dynamicFilterValue',
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: any, args: ValidationArguments) {
-          const { object } = args;
-          const filterType: 'specy' | 'status' | 'state' =
-            object['filter_type'];
-
-          if (filterType === 'specy' && !(value in Specy)) {
-            throw new BadRequestException({
-              error: {
-                message: `${value} is not a valid specy name`,
-                field: propertyName,
-                value,
-              },
-              code: HttpStatus.BAD_REQUEST,
-            });
-          }
-
-          if (filterType === 'state' && !(value in CharacterState)) {
-            throw new BadRequestException({
-              error: {
-                message: `${value} is not a valid state`,
-                field: propertyName,
-                value,
-              },
-              code: HttpStatus.BAD_REQUEST,
-            });
-          }
-
-          if (filterType === 'status' && !(value in CharacterStatus)) {
-            throw new BadRequestException({
-              error: {
-                message: `${value} is not a valid status`,
-                field: propertyName,
-                value,
-              },
-              code: HttpStatus.BAD_REQUEST,
-            });
-          }
-
-          return true;
-        },
-      },
-    });
-  };
-}
+import { IsEnum, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { DynamicFilterValue } from '../../decorators';
 
 export class GetAllParamsDtos {
   @IsEnum(['specy', 'state', 'status'])
